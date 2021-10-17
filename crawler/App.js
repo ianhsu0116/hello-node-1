6176e777dd19cd47eba94121d4c9e5f600e9668c
OlsonLin <len8501221@gmail.com>
2021-10-17 11:31:26 +0800

    Add Home(2021/10/17 11:31)

---
+++ crawler/App.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+
+let stockCode = "0050";
+let today = "20211017";
+let format = "json";
+
+// "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=" + format + "&date=" + today + "&stockNo=" + stockCode
+// `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=${format}&date=${today}&stockNo=${stockCode}`
+
+async function getStockData(format, today, stockCode) {
+  try {
+    let res = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          stockNo: stockCode,
+          date: today,
+          response: format,
+        },
+      }
+    );
+    console.log(res.data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("done");
+  }
+}
+
+getStockData(format, today, stockCode);
+
+// promise
+// axios
+//   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//     params: {
+//       response: format,
+//       date: today,
+//       stockNo: stockCode,
+//     },
+//   })
+//   .then((res) => {
+//     // HTTP response
+//     console.log(res.data);
+//   })
+//   .catch((e) => {
+//     console.error("發生錯誤啦", e);
+//   });
